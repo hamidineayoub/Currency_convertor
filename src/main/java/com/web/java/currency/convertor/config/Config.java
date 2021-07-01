e55d7a896584b05f008a3cce17b61281ebca6c7d
Ayoub Hamidine <hamidineayoub@gmail.com>
2021-07-01 12:21:09 +0100

    add server-time tracing

--- src/main/java/com/web/java/currency/convertor/config/Config.java
+++ src/main/java/com/web/java/currency/convertor/config/Config.java
@@ -1,5 +1,9 @@
 package com.web.java.currency.convertor.config;
 
+import io.github.sercasti.tracing.config.TracingConfig;
+import io.github.sercasti.tracing.core.Tracing;
+import io.github.sercasti.tracing.filter.TracingFilter;
+import io.github.sercasti.tracing.interceptor.TracingInterceptor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,5 +16,20 @@ public class Config {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplateBuilder().errorHandler(new RestTemplateResponseErrorHandler()).build();
+    }
+
+    @Bean
+    protected Tracing tracing() {
+        return TracingConfig.createTracing();
+    }
+
+    @Bean
+    protected TracingFilter tracingFilter() {
+        return new TracingFilter();
+    }
+
+    @Bean
+    protected TracingInterceptor tracingInterceptor() {
+        return new TracingInterceptor(tracing());
     }
 }
--- src/main/java/com/web/java/currency/convertor/controllers/CurrencyController.java
+++ src/main/java/com/web/java/currency/convertor/controllers/CurrencyController.java
@@ -3,12 +3,11 @@ package com.web.java.currency.convertor.controllers;
 import com.web.java.currency.convertor.models.CurrencyRateRequest;
 
 import com.web.java.currency.convertor.services.CurrencyService;
+import io.github.sercasti.tracing.Traceable;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
@@ -19,7 +18,9 @@ public class CurrencyController {
 
 
     @PostMapping("/calculate")
-    public ResponseEntity calculate(@RequestBody CurrencyRateRequest request){
+    @Traceable
+    public ResponseEntity calculate(@RequestBody CurrencyRateRequest request) {
         return currencyService.calculate(request);
     }
+
 }
