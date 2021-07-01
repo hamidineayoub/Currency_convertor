6c944f7b2485898eebe5fc9aaf13f5bcb040514d
Ayoub Hamidine <hamidineayoub@gmail.com>
2021-07-01 10:25:07 +0100

    add CrossOrigin

--- src/main/java/com/web/java/currency/convertor/controllers/CurrencyController.java
+++ src/main/java/com/web/java/currency/convertor/controllers/CurrencyController.java
@@ -5,11 +5,13 @@ import com.web.java.currency.convertor.models.CurrencyRateRequest;
 import com.web.java.currency.convertor.services.CurrencyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class CurrencyController {
 
     @Autowired
